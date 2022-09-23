78725906c2bc6e944ac2a9d566fb75c5aee4aea8
sean-morris <sean.smorris@gmail.com>
2022-09-23 02:24:52 +0000

    Week 7

---
+++ FewestMain.java
@@ -0,0 +1,11 @@
+public class FewestMain{
+    public static void main(String[] args) {
+        Fewest f = new Fewest();
+        System.out.println(f.fewestComparisons(35));
+        System.out.println(f.fewestComparisons(45));
+        System.out.println(f.fewestComparisons(75));
+        System.out.println(f.fewestComparisons(100));
+
+
+    }
+}
